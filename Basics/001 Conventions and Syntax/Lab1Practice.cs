376e1153470d78a2b3d5858ab364b2f35facdbd1
Nicholas Felsch <143032403+N-Felsch@users.noreply.github.com>
2023-09-04 12:50:35 -0600

    Practicing with CodeAcademy

--- Basics/001 Conventions and Syntax/Lab1Practice.cs
+++ Basics/001 Conventions and Syntax/Lab1Practice.cs
@@ -22,7 +22,9 @@ namespace HelloWorld
   {
     static void Main()
     {
-      Console.WriteLine("Nicholas");    
+      Console.WriteLine("Nicholas");  
+      int myNum = 15;
+      Console.WriteLine(myNum);  
      }
   }
 }
